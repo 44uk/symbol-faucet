22f5c24dc4b0b0041008337064ccd320e7438e44
Yoshiyuki Ieyama <yukku0423@gmail.com>
2019-06-06 11:59:10 +0900

    fix startup script

--- bin/create-env.sh
+++ bin/create-env.sh
@@ -2,11 +2,15 @@
 
 # get nemesis private key from generated addresses
 FILEPATH=/addresses/addresses.yaml
-NO=$(($(awk '/nemesis_addresses:/ {print NR}' $FILEPATH) +1))
-PRIVATE_KEY=$(awk -v no=$NO 'NR == no {print $3}' $FILEPATH)
-echo "NEM_PRIVATE_KEY=$PRIVATE_KEY" > .env
+if [ -f $FILEPATH ]; then
+  NO=$(($(awk '/nemesis_addresses:/ {print NR}' $FILEPATH) +1))
+  PRIVATE_KEY=$(awk -v no=$NO 'NR == no {print $3}' $FILEPATH)
+  echo "NEM_PRIVATE_KEY=$PRIVATE_KEY" > .env
+fi
 
 # get generation hash from generated block
 BLOCKPATH=/data/00000/00001.dat
-GENERATION_HASH=$(xxd -p -c 32 -s 5246 -l 32 $BLOCKPATH | tr "[:lower:]" "[:upper:]")
-echo "NEM_GENERATION_HASH=$GENERATION_HASH" >> .env
+if [ -f $BLOCKPATH ]; then
+  GENERATION_HASH=$(xxd -p -c 32 -s 5246 -l 32 $BLOCKPATH | tr "[:lower:]" "[:upper:]")
+  echo "NEM_GENERATION_HASH=$GENERATION_HASH" >> .env
+fi
