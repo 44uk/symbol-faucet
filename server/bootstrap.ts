59f0399c1bf360f331cb60ba936cae508b651e54
Yoshiyuki Ieyama <yukku0423@gmail.com>
2020-04-22 09:14:15 +0900

    update bootstrap

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -1,15 +1,14 @@
 import {
   Account,
   NetworkType,
-  MosaicId
+  MosaicId,
+  RepositoryFactoryHttp
 } from 'symbol-sdk'
 
-import { env } from "./libs"
+import { env, retryWithDelay } from "./libs"
 
 import {
   MosaicService,
-  BlockService,
-  NodeService
 } from './services'
 
 export interface IAppConfig {
@@ -36,45 +35,58 @@ export interface IAppConfig {
   FAUCET_ACCOUNT: Account
 }
 
+const repoFactory = new RepositoryFactoryHttp(env.API_URL)
+const networkRepo = repoFactory.createNetworkRepository()
+const namespaceRepo = repoFactory.createNamespaceRepository()
+
 export const init = async () => {
   let generationHash = env.GENERATION_HASH || ""
   if (!/[0-9A-Fa-f]{64}/.test(generationHash)) {
-    const blockService = new BlockService(env.API_URL)
-    generationHash = await blockService
-      .getGenerationHash()
-      .toPromise()
-    if (generationHash == null) {
+    try {
+      generationHash = await repoFactory.getGenerationHash()
+        .pipe(retryWithDelay({ delay: 5000 }))
+        .toPromise()
+    } catch(error) {
+      console.error(error)
       throw new Error('Failed to get GenerationHash from API Node')
     }
     console.info(`Get GenerationHash from API Node: "${generationHash}"`)
   }
 
-  let networkType = env.NETWORK_TYPE || ""
-  if (!/(MIJIN_TEST|MIJIN|TEST_NET|MAIN_NET)/.test(networkType)) {
-    const nodeService = new NodeService(env.API_URL)
-    networkType = await nodeService
-      .getNetworkType()
-      .toPromise()
-      .catch(error => "")
-    if (networkType === "") {
+  let networkType: NetworkType
+  if (/(MIJIN_TEST|MIJIN|TEST_NET|MAIN_NET)/.test(env.NETWORK_TYPE)) {
+    // @ts-ignore
+    networkType = NetworkType[env.NETWORK_TYPE]
+    console.info(`Set NetworkType from ENV: "${env.NETWORK_TYPE}"`)
+  } else {
+    try {
+      networkType = await repoFactory.getNetworkType()
+        .pipe(retryWithDelay({ delay: 5000 }))
+        .toPromise()
+
+    } catch(error) {
+      console.error(error)
       throw new Error('Failed to get NetworkType from API Node')
     }
-    console.info(`Get NetworkType from API Node: "${networkType}"`)
+    console.info(`Get NetworkType from API Node: "${NetworkType[networkType]}"`)
   }
 
   const mosaicService = new MosaicService(env.API_URL)
   let mosaicId: MosaicId | null
-  if (!/[0-9A-Fa-f]{6}/.test(env.MOSAIC_ID)) {
-    mosaicId = await mosaicService
-      .getLinkedMosaicId(env.MOSAIC_ID)
-      .toPromise()
-      .catch(error => null)
-    if (mosaicId == null) {
-      throw new Error(`Failed to get MosaicID(${env.MOSAIC_ID}) from API Node`)
+  if (/[0-9A-Fa-f]{6}/.test(env.MOSAIC_ID)) {
+    mosaicId = new MosaicId(env.MOSAIC_ID)
+  } else {
+    try {
+      const networkProps = await networkRepo.getNetworkProperties()
+        .pipe(retryWithDelay({ delay: 5000 }))
+        .toPromise()
+      const currencyMosaicId = (networkProps.chain.currencyMosaicId || "").replace('0x', '').replace(/'/g, '')
+      mosaicId = new MosaicId(currencyMosaicId)
+    } catch(error) {
+      console.error(error)
+      throw new Error(`Failed to get MosaicID from API Node`)
     }
     console.info(`Get MosaicID from API Node: "${mosaicId.toHex()}"`)
-  } else {
-    mosaicId = new MosaicId(env.MOSAIC_ID)
   }
 
   let mosaicFQN = (await mosaicService
@@ -83,17 +95,16 @@ export const init = async () => {
   ).join(",")
   console.info(`Get MosaicFQN from API Node: "${mosaicFQN}"`)
 
-  const config: IAppConfig = { ...env,
-      // @ts-ignore WIP
-    NETWORK_TYPE: NetworkType[networkType],
+  const config: IAppConfig = {
+    ...env,
+    NETWORK_TYPE: networkType,
     GENERATION_HASH: generationHash,
     MOSAIC_FQN: mosaicFQN,
     MOSAIC_ID: mosaicId,
     FAUCET_ACCOUNT: Account.createFromPrivateKey(
       env.PRIVATE_KEY as string,
-      // @ts-ignore WIP
-      NetworkType[networkType]
-    )
+      networkType
+    ),
   }
   console.debug({ config })
   return config
